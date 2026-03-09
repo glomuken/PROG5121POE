4c28c96ccfe8e224b39d5196c19fbf7f2cfec663
Kamwanya Mukendi <136966616+glomuken@users.noreply.github.com>
2026-03-09 18:11:50 +0200

    files
    files created

---
+++ chat-app/src/main/java/com/mycompany/chat/app/ChatApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chat.app;
+
+/**
+ *
+ * @author Dell
+ */
+public class ChatApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
